896a67bc4a2291b52a1d0400558182b66da3246f
yassirjoudar <yassirjoudar2005@gmail.com>
2023-07-13 15:31:38 -0700

    hollo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
+/**
+ * discription: print "Programming is like building a multilingual puzzle
+ *
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
