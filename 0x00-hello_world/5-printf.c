5661651f3c289b2c199db1d07eda2ae6d0bdb75a
yassirjoudar <yassirjoudar2005@gmail.com>
2023-07-13 15:49:56 -0700

    momo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
- *  Description: prints with proper grammar, but the outcome is a piece of art
+ * main - Entry point
  *
+ * Description:
+ * prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
