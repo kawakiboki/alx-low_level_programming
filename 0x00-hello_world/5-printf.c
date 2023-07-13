cdf94979373d5ff0c01c4fd10aeea10512f8c1ab
yassirjoudar <yassirjoudar2005@gmail.com>
2023-07-13 15:45:43 -0700

    hollo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  Description: prints with proper grammar, but the outcome is a piece of art
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
